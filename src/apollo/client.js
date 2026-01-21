b81c6fc5505d1af0a7e9381acfda3b1b09b6fe68
Vipul <vipulmaske2939@gmail.com>
2026-01-21 12:49:07 +0530

    update GraphQL URI for Azure backend

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -2,8 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  // uri: import.meta.env.VITE_GRAPHQL_URI,
-  uri: "http://localhost:7000/graphql",
+  uri: import.meta.env.VITE_GRAPHQL_URI,
 });
 
 const authLink = setContext((_, { headers }) => {
